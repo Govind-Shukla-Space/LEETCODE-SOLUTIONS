c04e17e337030f3f9b92bc6ba2dfdef49b1bf9fd
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-05-25 19:13:43 +0530

    Update 0450-delete-node-in-a-bst.java

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -13,35 +13,6 @@
  *     }
  * }
  */
-class Solution1 {
-    // O(height)
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null)
-        return root;
-        else if(key<root.val){
-        root.left=deleteNode(root.left,key);
-        return root;
-        }
-        else if(key>root.val){
-        root.right=deleteNode(root.right,key);
-        return root;
-        }
-        else if(root.left==null){
-            return root.right;
-        }
-        else if(root.right==null){
-        return root.left;
-        }
-        else{
-            TreeNode q=root.left;
-            while(q.right!=null)
-            q=q.right;
-            root.val=q.val;
-            root.left=deleteNode(root.left,q.val);
-            return root;
-        }
-    }
-}
 class Solution{
     TreeNode f1(TreeNode root,int key){
         if(root==null)
