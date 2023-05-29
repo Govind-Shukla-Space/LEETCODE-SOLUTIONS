0f5921f4e7f125f521a494754028352d0f35a3bd
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-05-29 22:31:39 +0530

    Time: 188 ms (5.03%), Space: 45.7 MB (35.87%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -14,18 +14,36 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> ar=new ArrayList<Integer>();
-    void fun(TreeNode root){
+    ArrayList<Integer> ar1=new ArrayList<Integer>();
+    ArrayList<Integer> ar2=new ArrayList<Integer>();
+    ArrayList<Integer> ar3=new ArrayList<Integer>();
+    void fun(TreeNode root,int f){
         if(root==null)
         return ;
-        fun(root.left);
-        ar.add(root.val);
-        fun(root.right);
+        fun(root.left,f);
+        if(f==0)
+        ar1.add(root.val);
+        else
+        ar2.add(root.val);
+        fun(root.right,f);
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        fun(root1);
-        fun(root2);
-        Collections.sort(ar);
-        return ar;
+        fun(root1,0);
+        fun(root2,1);
+        int i=0,j=0;
+        // Collections.sort(ar);
+        for(int p=0;p<ar2.size();p++)
+        System.out.println(ar2.get(p));
+        while(i<ar1.size()&&j<ar2.size()){
+            if(ar1.get(i)<=ar2.get(j))
+            ar3.add(ar1.get(i++));
+            else if(ar1.get(i)>ar2.get(j))
+            ar3.add(ar2.get(j++));
+        }
+        while(i<ar1.size())
+        ar3.add(ar1.get(i++));
+        while(j<ar2.size())
+        ar3.add(ar2.get(j++));
+        return ar3;
     }
 }
