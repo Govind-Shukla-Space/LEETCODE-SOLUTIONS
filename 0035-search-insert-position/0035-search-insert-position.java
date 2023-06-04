43c775d05f0566946c38acd55eff63f29f65127d
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-06-04 10:01:13 +0530

    Time: 0 ms (100%), Space: 43.1 MB (22.39%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int a=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>target){
+                return i;
+            }
+            if(nums[i]==target)
+            return i;
+        }
+        return nums.length;
+    }
+}
