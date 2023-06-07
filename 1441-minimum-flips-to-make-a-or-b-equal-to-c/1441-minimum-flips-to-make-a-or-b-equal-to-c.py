4582ff5e9c7b72428328bd03413409d52cc0b59f
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-06-07 17:31:37 +0530

    Time: 64 ms (6.59%), Space: 16.4 MB (25.27%) - LeetHub

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,42 @@
+def add(s,l):
+    s1=""
+    for i in range(l):
+        s1+='0'
+    return s1+s
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        s1=bin(a).replace("0b", "")
+        s2=bin(b).replace("0b", "")
+        s3=bin(c).replace("0b", "")
+        l=max(len(s1),len(s2),len(s3))
+        c1=0
+        s1=add(s1,l-len(s1))
+        s2=add(s2,l-len(s2))
+        s3=add(s3,l-len(s3))
+        # if len(s1)>len(s2):
+        # s2=add(s2,len(s1)-len(s2))
+        # s3=add(s3,len(s1)-len(s3))
+        # # else:
+        # s1=add(s1,len(s2)-len(s1))
+        # s3=add(s3,len(s2)-len(s3))
+        lo=[]
+        lz=[]
+        print(s1)
+        print(s2)
+        print(s3)
+        for i in range(len(s3)):
+            if s3[i]=='1':
+                lo.append(i)
+            if s3[i]=='0':
+                lz.append(i)
+        for i in lo:
+            if s1[i]=='0' and s2[i]=='0':
+                c1+=1
+        for j in lz:
+            if s1[j]=='1' and s2[j]=='1':
+                c1+=2
+            elif s1[j]=='1' or s2[j]=='1':
+                c1+=1
+        # print(s1)
+        # print(s2)
+        return c1
