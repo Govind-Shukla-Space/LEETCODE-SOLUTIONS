fe22e66d0c9015ab31e43f1c7444a112d51ab532
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-06-07 18:58:51 +0530

    Time: 0 ms (100%), Space: 39.4 MB (58.18%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,15 +1,26 @@
 class Solution {
-    int f(int n){
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
-    }
-    public int fib(int n) {
+    int f(int n,int dp[]){
         if(n==0)
         return 0;
-        return f(n);
+        if(n==1)
+        return 1;
+        // if (n==2)
+        // return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        else{
+            dp[n]=f(n-1,dp)+f(n-2,dp);
+             
+            return dp[n]; 
+            }
+    }
+    public int fib(int n) {
+        
+        int dp[]=new int[n+1];
+        // dp[0]=0;
+        for (int i=0;i<dp.length;i++)
+        dp[i]=-1;
+        return f(n,dp);
+
     }
 }
