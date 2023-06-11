1142c28875a70555bc8cfc06c09ecf476b472c3f
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-06-11 12:34:02 +0530

    Time: 2 ms (50.86%), Space: 54.2 MB (15.21%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,33 @@
+class Solution1 {//tle
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]=new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            int p=1;
+            for(int j=0;j<nums.length;j++){
+                if(i!=j)
+                p*=nums[j];
+            }
+            ans[k++]=p;
+        }
+        return ans;
+    }
+}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]=new int[nums.length];
+        int suff[]=new int [nums.length];
+        int pre[]=new int[nums.length];
+        pre[0]=1;
+        suff[nums.length-1]=1;
+        for(int i=1;i<nums.length;i++){
+            pre[i]=pre[i-1]*nums[i-1];
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            suff[i]=suff[i+1]*nums[i+1];
+        }
+        for(int i=0;i<nums.length;i++)
+        ans[i]=pre[i]*suff[i];
+        return ans;
+    }
+}
