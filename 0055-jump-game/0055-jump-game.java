1c67b36e9646bc328c5909604ad0615f6be980d7
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-06-19 19:01:36 +0530

    Time: 636 ms (6.89%), Space: 44.2 MB (44.49%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,72 @@
+class Solution1 {
+    //TLE
+    boolean jump(int pos,int nums[]){
+        if(pos==nums.length-1)
+        return true;
+        int n=nums.length;
+        int far=Math.min(n-1,pos+nums[pos]);
+        for(int next=pos+1;next<=far;next++){
+            if(jump(next,nums)==true)
+            return true;
+        }
+        return false;
+    }
+    public boolean canJump(int[] nums) {
+        return jump(0,nums);
+    }
+}
+class Solution2 {
+    //dp
+    boolean jump(int pos,int nums[],int dp[]){
+        if(pos==nums.length-1){
+            return true;
+        }
+        dp[pos]=1;
+        // return true;
+        int n=nums.length;
+        int far=Math.min(n-1,pos+nums[pos]);
+        for(int next=pos+1;next<=far;next++){
+            if(dp[next]==1){
+                continue;
+            }
+            if(jump(next,nums,dp))
+            return true;
+
+        }
+        return false;
+    }
+    public boolean canJump(int[] nums) {
+        int dp[]=new int[nums.length];
+        for(int i=0;i<dp.length;i++)
+        dp[i]=-1;
+        return jump(0,nums,dp);
+    }
+}
+class Solution {
+    //pure dp
+    boolean jump(int pos,int nums[],boolean dp[]){
+        if(pos==nums.length-1){
+            dp[pos]=true;
+        }
+        // return true;
+        int n=nums.length;
+        dp[n-1]=true;
+        
+        for(int i=n-2;i>=0;i--){
+            int far=Math.min(n-1,i+nums[i]);
+            for(int j=i+1;j<=far;j++){
+                if (dp[j]==true)
+                dp[i]=true;
+            }
+            if(dp[i]!=true)
+            dp[i]=false;
+        }
+        return dp[0];
+    }
+    public boolean canJump(int[] nums) {
+        boolean dp[]=new boolean[nums.length];
+        for(int i=0;i<dp.length;i++)
+        dp[i]=false;
+        return jump(0,nums,dp);
+    }
+}
