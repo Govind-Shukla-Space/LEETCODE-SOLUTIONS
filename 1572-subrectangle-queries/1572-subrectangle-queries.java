315d422430be5e7fef7772401d54db24dc86ca16
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-07-03 18:37:33 +0530

    Time: 30 ms (19.54%), Space: 47.3 MB (37.35%) - LeetHub

--- 1572-subrectangle-queries/1572-subrectangle-queries.java
+++ 1572-subrectangle-queries/1572-subrectangle-queries.java
@@ -2,11 +2,7 @@ class SubrectangleQueries {
     int rect[][];
     public SubrectangleQueries(int[][] rectangle) {
         this.rect=rectangle;
-        // int n=rectangle.length,m=rectangle[0].length;
-        // int rect[][]=new int[n][m];
-        // for(int i=0;i<n;i++)
-        // for(int j=0;j<m;j++)
-        // rect[i][j]=rectangle[i][j];
+       
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
