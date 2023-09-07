4e951d49fa148e034a697d358f0f294e841d247b
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-07 22:51:51 +0530

    Time: 1 ms (50.15%), Space: 44.1 MB (91.54%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         if(mat[0].length*mat.length!=r*c)
         return mat;
@@ -19,5 +19,25 @@ class Solution {
         }
 
         return arr;
+    }
+}
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(mat[0].length*mat.length!=r*c)
+        return mat;
+        int arr[]=new int[r*c];
+        int k=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++)
+            arr[k++]=mat[i][j];
+        }
+        int b[][]=new int[r][c];
+        k=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                b[i][j]=arr[k++];
+            }
+        }
+        return b;
     }
 }
