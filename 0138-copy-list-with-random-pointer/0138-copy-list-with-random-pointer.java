d016baf03e9008be5b8c4efc4b27dfd7ed6d6c6a
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-17 12:35:23 +0530

    Time: 8 ms (7.78%), Space: 44 MB (8.93%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,7 +13,7 @@ class Node {
 }
 */
 
-class Solution {
+class Solution1 {
     public Node copyRandomList(Node head) {
         Node dummy=new Node(-1);
         Node pre=dummy;
@@ -33,5 +33,62 @@ class Solution {
             t1=t1.next;
         }
         return dummy.next;
+    }
+}
+
+class Solution {
+    Node copy(Node head){
+        Node t1=head;
+        while (t1!=null){
+            
+            Node w=t1.next;
+            Node q=new Node(t1.val);
+            t1.next=q;
+            q.next=w;
+            t1=w;
+        }
+        return head;
+    }
+    void random(Node head){
+        Node t1=head;
+        while(t1!=null){
+            if(t1.random!=null)
+            t1.next.random=t1.random.next;
+            t1=t1.next.next;
+        }
+    }
+    Node finall(Node head){
+        Node t1=head,t3=new Node(-1),t2=t3;
+        // while(t2!=null && t2.next!=null){
+        while(t1!=null){    
+            // System.out.print(" "+t2.val+" ");
+            Node f=t1.next.next;
+            Node c=t1.next;
+            t3.next=c;
+            t3=c;
+            t1.next=f;
+            t1=f;
+            // t2.next=t2.next.next;
+            // t2=t2.next;
+        }
+        return t2.next;
+    }
+        void print(Node head){
+        Node t=head;
+        while(t!=null){
+            System.out.println(t.val);
+            t=t.next;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        // Node t=head;
+        Node t2=copy(head);
+        // print(t);
+        random(t2);
+        print(t2);
+        return finall(t2);
+        // print(t.next);
+        // return t.next;
     }
 }
