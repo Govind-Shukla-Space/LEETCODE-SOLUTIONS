51287509e25ca0f940ef895abae4cb02918c849a
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-21 11:36:56 +0530

    Time: 0 ms (100%), Space: 41 MB (79.01%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution1 {
     public ListNode reverseList(ListNode head) {
         ListNode curr=head,pre=null,next=null;
         while(curr!=null){
@@ -18,5 +18,18 @@ class Solution {
             curr=next;
         }
         return pre;
+    }
+}
+class Solution {
+    ListNode reverse(ListNode head){
+        if(head==null||head.next==null)
+        return head;
+        ListNode h=reverse(head.next);
+        head.next.next=head;
+        head.next=null;
+        return h;
+    }
+    public ListNode reverseList(ListNode head) {
+        return reverse(head);
     }
 }
