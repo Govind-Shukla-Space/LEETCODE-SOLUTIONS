b1a693278f493c4d7a7d42814a59c08b54f33cd7
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-22 18:36:18 +0530

    Time: 438 ms (90.88%), Space: 0B (100%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,5 +1,6 @@
-update Salary 
-set sex=case
-when sex='f' then 'm'
-when sex='m' then 'f'
-end
+# update Salary 
+# set sex=case
+# when sex='f' then 'm'
+# when sex='m' then 'f'
+# end
+update Salary set sex=if(sex='m','f','m')
