91b39e1cb96b3a128538f40faf60feefc7a24611
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-22 13:30:08 +0530

    Time: 1846 ms (5.02%), Space: 46.4 MB (41.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int subarraySum(int[] nums, int k) {
         int c=0;
         for(int i=0;i<nums.length;i++){
@@ -10,5 +10,55 @@ class Solution {
             }
         }
         return c;
+    }
+}
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int pre[]=new int[nums.length+1];
+        pre[0]=0;
+        // pre[0]=nums[0];
+        int c=0;
+        for(int i=1;i<=nums.length;i++)
+        pre[i]=nums[i-1]+pre[i-1];
+        for(int i=0;i<pre.length;i++)
+        System.out.print(pre[i]+" ");
+        for(int i=0;i<pre.length;i++){
+            for(int j=i+1;j<pre.length;j++){
+                // System.out.println(pre[j]+" "+pre[i]);
+                if((pre[j]-pre[i])==k)
+                c+=1;
+            }
+        }
+        // if(c==0)
+        return c;
+        // return c+1;
+    }
+}
+class Solution2 {
+    public int subarraySum(int[] nums, int k) {
+        int pre[]=new int[nums.length+1];
+        pre[0]=0;
+        pre[1]=nums[0];
+        int c=0;
+        for(int i=2;i<nums.length+1;i++)
+        pre[i]=nums[i-1]+pre[i-1];
+        for(int i=0;i<pre.length;i++)
+        System.out.print(pre[i]+" ");
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int i=0;i<pre.length;i++){
+            if (set.contains(pre[i]-k))
+            c++;
+            else{
+                set.add(pre[i]);
+            }
+            // for(int j=i+1;j<pre.length;j++){
+            //     System.out.println(pre[j]+" "+pre[i]);
+            //     if((pre[j]-pre[i])==k)
+            //     c+=1;
+            // }
+        }
+        // if(c==0)
+        return c;
+        // return c+1;
     }
 }
