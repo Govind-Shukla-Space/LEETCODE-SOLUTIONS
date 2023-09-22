5890b590b99c25d6514780a200c2fea2722ff193
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-22 14:05:13 +0530

    Time: 315 ms (29.76%), Space: 46.7 MB (24.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -12,7 +12,7 @@ class Solution1 {
         return c;
     }
 }
-class Solution {
+class Solution2 {
     public int subarraySum(int[] nums, int k) {
         int pre[]=new int[nums.length+1];
         pre[0]=0;
@@ -34,23 +34,30 @@ class Solution {
         // return c+1;
     }
 }
-class Solution2 {
+class Solution {
     public int subarraySum(int[] nums, int k) {
         int pre[]=new int[nums.length+1];
         pre[0]=0;
-        pre[1]=nums[0];
+        // pre[1]=nums[0];
         int c=0;
-        for(int i=2;i<nums.length+1;i++)
+        for(int i=1;i<=nums.length;i++)
         pre[i]=nums[i-1]+pre[i-1];
         for(int i=0;i<pre.length;i++)
         System.out.print(pre[i]+" ");
-        HashSet<Integer> set=new HashSet<Integer>();
-        for(int i=0;i<pre.length;i++){
-            if (set.contains(pre[i]-k))
-            c++;
-            else{
-                set.add(pre[i]);
+        HashMap<Integer,Integer> set=new HashMap<Integer,Integer>();
+        int sum=0;
+        set.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if (set.containsKey(sum-k))
+            {
+                c+=set.get(sum-k);
             }
+                set.put(sum,set.getOrDefault(sum,0)+1);
+            // }
+            // else{
+            //     set.put(sum,0);
+            // }
             // for(int j=i+1;j<pre.length;j++){
             //     System.out.println(pre[j]+" "+pre[i]);
             //     if((pre[j]-pre[i])==k)
