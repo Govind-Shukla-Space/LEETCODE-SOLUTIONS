d1451af565707cdc879fdaa42faecb0df4d2ac79
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-23 14:16:56 +0530

    Time: 0 ms (100%), Space: 40.4 MB (85.77%) - LeetHub

---
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMiddleIndex(int[] arr) {
+        int n=arr.length;
+        int a[]=new int[n];
+        int b[]=new int[n];
+        a[0]=arr[0];
+        b[n-1]=arr[n-1];
+        for(int i=1;i<arr.length;i++)
+        a[i]=a[i-1]+arr[i];
+        for (int j=n-2;j>=0;j--) 
+        b[j]=b[j+1]+arr[j];
+        // for(int i=n-1;i>0;i--)
+        // System.out.println(b[i]);
+        for(int i=0;i<arr.length;i++){
+            if(a[i]-b[i]==0)
+            return i;
+        }
+        
+        return -1;
+    }
+}
