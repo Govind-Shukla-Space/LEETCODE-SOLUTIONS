0be7d3929b115a41a515cbd8663f162f5c2eec91
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-25 21:37:44 +0530

    Time: 19 ms (45.1%), Space: 42.5 MB (99.1%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -8,52 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution1 {
-    public ListNode insertionSortList(ListNode head) {
-        if(head==null || head.next==null)
-        return head;
-        ListNode r=insertionSortList(head.next);
-        if(head.val<=r.val){
-            head.next=r;
-            return head;
-        }
-        ListNode prev=r;
-        while(prev.next!=null && prev.next.val<head.val){
-            prev=prev.next;
-        }
-        head.next=prev.next;
-        prev.next=head;
-        return r;
-    }
-}
-
-//top-down
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        if(head==null || head.next==null)
+        if(head==null||head.next==null)
         return head;
-        ListNode lastnode=head.next;
-        ListNode sortedArray=head;
+        ListNode t1=head.next,next=null;
+        ListNode t2=null,sort=head;
         head.next=null;
-        while(lastnode!=null){
-            //sortedArray is f(n-1)
-            //lastnode is nth node
-            //insert lastnode in the sortedArray
-            ListNode nextnode=lastnode.next;
-            if(lastnode.val<=sortedArray.val){
-                lastnode.next=sortedArray;
-                sortedArray=lastnode;
+        while (t1!=null){
+            t2=t1.next;
+            if(t1.val<=sort.val){
+                t1.next=sort;
+                sort=t1;
             }
             else{
-                ListNode prev=sortedArray;
-                while(prev.next!=null && prev.next.val<lastnode.val){
-                    prev=prev.next;
-                }
-                lastnode.next=prev.next;
-                prev.next=lastnode;
+                next=sort;
+                while(next.next!=null&& next.next.val<t1.val)
+                    next=next.next;
+                t1.next=next.next;
+                next.next=t1;
             }
-            lastnode=nextnode;
+            t1=t2;
         }
-        return sortedArray;
+        return sort;
     }
 }
