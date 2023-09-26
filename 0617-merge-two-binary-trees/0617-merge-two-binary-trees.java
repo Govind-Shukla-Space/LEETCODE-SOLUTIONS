50e16169b606798a41e3c10aa8759567f93fff7d
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-26 18:36:51 +0530

    Time: 1 ms (19%), Space: 43.7 MB (52.07%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -15,29 +15,11 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-    if(root1==null && root2==null)
-    return null;
-    if(root1==null)
-    return root2;
-    if(root2==null)
-    return root1;
-
-    Stack<TreeNode[]> stack=new Stack<TreeNode[]>();        
-    stack.push(new TreeNode[]{root1,root2});
-    while(!(stack.isEmpty())){
-        TreeNode t[]=stack.pop();
-        if (t[1]==null)
-        continue;
-        t[0].val=t[0].val+t[1].val;
-        if (t[0].left==null)
-        t[0].left=t[1].left;
-        else
-        stack.push(new TreeNode[]{t[0].left,t[1].left});
-        if(t[0].right==null)
-        t[0].right=t[1].right;
-        else
-        stack.push(new TreeNode[]{t[0].right,t[1].right});
-    }
-    return root1;
+        if(root1==null &&root2==null)
+        return null;
+        TreeNode root=new TreeNode(((root1!=null)?root1.val:0)+((root2!=null)?root2.val:0));
+        root.left=mergeTrees(((root1!=null)?root1.left:null),((root2!=null)?root2.left:null));
+        root.right=mergeTrees(((root1!=null)?root1.right:null),((root2!=null)?root2.right:null));
+        return root;
     }
 }
