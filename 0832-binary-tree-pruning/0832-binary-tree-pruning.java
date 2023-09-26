8cd4f63c854e972e44cddefd4437219e69f88434
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-26 18:54:52 +0530

    Time: 0 ms (100%), Space: 40.6 MB (8.16%) - LeetHub

--- 0832-binary-tree-pruning/0832-binary-tree-pruning.java
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.java
@@ -14,17 +14,14 @@
  * }
  */
 class Solution {
-    TreeNode fun(TreeNode root){
+    public TreeNode pruneTree(TreeNode root) {
         if(root==null)
         return null;
-        root.left=fun(root.left);
-        root.right=fun(root.right);
-        if(root.left==null&& root.right==null&&root.val==0)
+        
+        root.left=pruneTree(root.left);
+        root.right=pruneTree(root.right);
+        if(root.val==0&&root.left==null&&root.right==null)
         return null;
         return root;
-    }
-    public TreeNode pruneTree(TreeNode root) {
-        return fun(root);
-        
     }
 }
