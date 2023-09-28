fb8405e3026feaf75e95b5e9898070b7341a47ac
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-28 11:33:03 +0530

    Time: 19 ms (44.64%), Space: 43.4 MB (21.6%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -12,21 +12,22 @@ class Solution {
     public ListNode insertionSortList(ListNode head) {
         if(head==null||head.next==null)
         return head;
-        ListNode t1=head.next,next=null;
-        ListNode t2=null,sort=head;
-        head.next=null;
-        while (t1!=null){
+        ListNode t1=head.next,t2=null;
+        ListNode sort=head;
+        sort.next=null;
+        while(t1!=null){
             t2=t1.next;
             if(t1.val<=sort.val){
                 t1.next=sort;
                 sort=t1;
             }
             else{
-                next=sort;
-                while(next.next!=null&& next.next.val<t1.val)
-                    next=next.next;
-                t1.next=next.next;
-                next.next=t1;
+                ListNode pre=sort;
+                while(pre.next!=null && t1.val>pre.next.val){
+                    pre=pre.next;
+                }
+                t1.next=pre.next;
+                pre.next=t1;
             }
             t1=t2;
         }
