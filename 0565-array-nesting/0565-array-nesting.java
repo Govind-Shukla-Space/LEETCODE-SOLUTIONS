4113f0a246c3c1a516758a47c2adc5e3af66fde1
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-09-29 12:29:25 +0530

    Time: 4 ms (99.32%), Space: 56.6 MB (71.33%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -1,16 +1,14 @@
 class Solution {
     public int arrayNesting(int[] nums) {
-        int c=0,ma=0;
+        int ma=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==-1)
             continue;
-            int k,k1=nums[i];
-            c=1;
+            int k,k1=nums[i],c=1;
             while(k1!=i){
                 k=nums[k1];
                 nums[k1]=-1;
                 k1=k;
-                
                 c++;
             }
             ma=Math.max(ma,c);
