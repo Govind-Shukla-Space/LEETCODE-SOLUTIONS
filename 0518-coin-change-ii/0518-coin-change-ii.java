558d8c75d9ad671f6cf8999aa5cf1a777950f449
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-10-30 19:08:49 +0530

    Time: 14 ms (15.09%), Space: 45.5 MB (39.67%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,49 +1,113 @@
-class Solution1 {//tle
-    int f1(int i,int a,int[] coins){
-        if(i==coins.length){
-            if(a==0)
-            return 1;
-            else
+class Solution1{    
+    ArrayList<ArrayList<Integer>> arr=new ArrayList<ArrayList<Integer>>();
+    void generate(int i,int amount,int coins[],ArrayList<Integer> sub){
+        if (i>=coins.length){
+            arr.add(new ArrayList<Integer>(sub));
+            return ;
+        }
+        if(amount<0)
+        return;
+        else{
+            generate(i+1,amount,coins,sub);
+            sub.add(coins[i]);
+            generate(i,amount-coins[i],coins,sub);
+            sub.remove(sub.size()-1);
+        }
+    }
+    boolean valid(ArrayList<Integer> sub,int amount){
+        int s=0;
+        for(int i=0;i<sub.size();i++)
+            s+=sub.get(i);
+        return s==amount;
+    }
+    public int change(int amount, int[] coins) {
+        int count=0;
+        generate(0,amount,coins,new ArrayList<>());
+        // System.out.println(arr);
+        for(int i=0;i<arr.size();i++){
+            if(valid(arr.get(i),amount)){
+                count+=1;
+            }
+        }
+        return count;
+    }
+}
+
+class Solution2{
+    int count=0,amt;
+    void generate(int i,int amount,int coins[],ArrayList<Integer> sub){
+        if (i>=coins.length){
+            System.out.println(sub);
+            int s=0;
+            for(int j=0;j<sub.size();j++)
+                s+=sub.get(j);    
+            if(s==amt)
+            count+=1;
+            return ;
+        }
+        if(amount<0)
+        return;
+        else{
+            generate(i+1,amount,coins,sub);
+            sub.add(coins[i]);
+            generate(i,amount-coins[i],coins,sub);
+            sub.remove(sub.size()-1);
+        }
+    }
+    public int change(int amount, int[] coins) {
+        amt=amount;
+        generate(0,amount,coins,new ArrayList<>());
+        return count;
+    }
+}
+
+class Solution3{
+    int generate(int i,int amount,int coins[]){
+        if (i==coins.length){
             return 0;
         }
-        else if (a<0)
-        return 0;
+        if(amount<0)
+            return 0;
+        if(amount==0)
+            return 1;
         else{
-            int ans=f1(i+1,a,coins);
-            ans+=f1(i,a-coins[i],coins);
+            int ans=generate(i+1,amount,coins);
+            ans+=generate(i,amount-coins[i],coins);
             return ans;
         }
     }
     public int change(int amount, int[] coins) {
-        return f1(0,amount,coins);
-
+        return generate(0,amount,coins);
+        // return count;
     }
 }
-class Solution {
-    int f1(int i,int a,int[] coins,int tab[][]){
-        if(tab[i][a]!=Integer.MIN_VALUE)
-        return tab[i][a];
-        if(i==coins.length){
-            if(a==0){
-            return tab[i][a]=1;
-            }
-            else{
-                return tab[i][a]=0;
-            }
+
+class Solution{
+    int generate(int i,int amount,int coins[],int tlb[][]){
+        if(tlb[i][amount]!=Integer.MIN_VALUE)
+        return tlb[i][amount];
+        if (i==coins.length){
+            return 0;
+        }
+        if(amount<0){
+            return tlb[i][amount]=0;
+        }
+        if(amount==0){
+            return tlb[i][amount]=1;
         }
         else{
-            int ans=f1(i+1,a,coins,tab);
-            if(a>=coins[i])
-            ans+=f1(i,a-coins[i],coins,tab);
-            return tab[i][a]=ans;
+            int ans=generate(i+1,amount,coins,tlb);
+            if(amount>=coins[i])
+            ans+=generate(i,amount-coins[i],coins,tlb);
+            return tlb[i][amount]=ans;
         }
     }
     public int change(int amount, int[] coins) {
-        int tab[][]=new int[coins.length+1][amount+1];
-        for(int i=0;i<tab.length;i++)
-        for(int j=0;j<tab[0].length;j++)
-        tab[i][j]=Integer.MIN_VALUE;
-        return f1(0,amount,coins,tab);
-
+        int tlb[][] =new int[coins.length+1][amount+1];
+        for(int i=0;i<tlb.length;i++)
+            for(int j=0;j<tlb[0].length;j++)
+                tlb[i][j]=Integer.MIN_VALUE;
+        return generate(0,amount,coins,tlb);
+        // return count;
     }
 }
