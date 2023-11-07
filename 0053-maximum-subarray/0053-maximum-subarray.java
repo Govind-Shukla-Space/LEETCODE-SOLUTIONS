aadf57310b5273683014561a91678963105728e6
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2023-11-07 17:47:31 +0530

    Time: 9 ms (7.61%), Space: 59.4 MB (48.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -48,7 +48,8 @@ class Solution3 {
         return max;
     }
 }
-class Solution {
+// O(n) ,O(1)
+class Solution4 {
     public int maxSubArray(int[] nums) {
         int n=nums.length;
         int max=Integer.MIN_VALUE;
@@ -61,5 +62,64 @@ class Solution {
             low=ps;
         }
         return max;
+    }
+}
+class Solution5 {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int max=Integer.MIN_VALUE;
+        int ps=0;
+        for(int i=0;i<n;i++){
+            ps+=nums[i];
+            max=Math.max(max,ps);
+            if (ps<0)
+            ps=0;
+        }
+        return max;
+    }
+}
+class Solution6 {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int max=Integer.MIN_VALUE;
+        int ps=0;
+        for(int i=0;i<n;i++){
+            ps+=nums[i];
+            max=Math.max(max,ps);
+            if (ps<0)
+            ps=0;
+        }
+        return max;
+    }
+}
+// Divide and Conquer
+class Solution {
+    int f1(int i,int j,int nums[]){
+        if(i==j)
+        return nums[i];
+        int mid=(i+j)/2;
+        int left=f1(i,mid,nums);
+        int right=f1(mid+1,j,nums);
+        int ans=Math.max(left,right);
+        return Math.max(ans,f2(i,mid,j,nums));
+    }
+    int f2(int i,int mid,int j,int nums[]){
+        int suf=nums[mid];
+        int sum=nums[mid];
+        for(int k=mid-1;k>=i;k--){
+            sum+=nums[k];
+            suf=Math.max(sum,suf);
+        }
+        int pref=nums[mid+1];
+        sum=nums[mid+1];
+        for(int k=mid+2;k<=j;k++){
+            sum+=nums[k];
+            pref=Math.max(sum,pref);
+        }
+        return suf+pref;
+    }
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        return f1(0,n-1,nums);
     }
 }
