d4758a4035bcfbf3d358818a32d309d0ab23d4c2
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2024-08-27 14:47:45 +0530

    Time: 5 ms (66.82%), Space: 44.8 MB (64.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,10 +1,10 @@
 class Solution {
-    int fun(int i,int j,String s1,String s2,Integer dp[][]){
+    int fun(int i,int j,String s1,String s2,int dp[][]){
         if(i<0)
             return j+1;
         if(j<0)
             return i+1;
-        if(dp[i][j]!=null)
+        if(dp[i][j]!=-1)
             return dp[i][j];
         if(s1.charAt(i)==s2.charAt(j))
             return dp[i][j]=fun(i-1,j-1,s1,s2,dp);
@@ -14,7 +14,26 @@ class Solution {
         return dp[i][j]=Math.min(ins,Math.min(del,rep));
     }
     public int minDistance(String word1, String word2) {
-        Integer dp[][]=new Integer[word1.length()][word2.length()];
-        return fun(word1.length()-1,word2.length()-1,word1,word2,dp);
+        int dp[][]=new int[word1.length()+1][word2.length()+1];
+        
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp.length;j++)
+        //         dp[i][j]=-1;
+        // }
+        // return fun(word1.length()-1,word2.length()-1,word1,word2,dp);
+        for(int j=1;j<dp[0].length;j++)
+            dp[0][j]=j;
+        for(int i=1;i<dp.length;i++)
+            dp[i][0]=i;
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else{
+                    dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
     }
 }
