7c9a234a3d58fe5d161b6810bfce39c94f2c00fa
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2024-10-08 14:55:11 +0530

    Time: 13 ms (47.05%), Space: 41.5 MB (86.20%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,4 +1,21 @@
 class Solution1 {
+    int fun(int i,int n,int k,int t){
+        if(i==n){
+            if(t==0) return 1;
+            return 0;
+        }
+        if(t<0)
+            return 0;
+        int s=0;
+        for(int j=1;j<=k;j++)
+            s=(s+fun(i+1,n,k,t-j))%(int)(1e9+7);
+        return s;
+    }
+    public int numRollsToTarget(int n, int k, int target) {
+        return fun(0,n,k,target);
+    }
+}
+class Solution2 {
     int fun(int i,int n,int k,int t,HashMap<Pair<Integer,Integer>,Integer> map){
         if(i==n){
             if(t==0) return 1;
@@ -18,8 +35,8 @@ class Solution1 {
         HashMap<Pair<Integer,Integer>,Integer> map=new HashMap<Pair<Integer,Integer>,Integer>();
         return fun(0,n,k,target,map);
     }
-}
-class Solution {
+} //memo -> hashMap
+class Solution3 {
     int fun(int i,int n,int k,int t,Integer dp[][]){
         if(i==n){
             if(t==0) return 1;
@@ -37,5 +54,24 @@ class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         Integer dp[][]=new Integer[n+1][target+1];
         return fun(0,n,k,target,dp);
+    }
+} //memo -> array
+class Solution {
+ public int numRollsToTarget(int n, int k, int target) {
+        int dp[][]=new int[n+1][target+1];
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                int ans=0;
+                for(int f=1;f<=k;f++){
+                    if(j-f>=0)
+                        ans=(ans+dp[i-1][j-f])%(int)(1e9+7);
+                }
+                
+                dp[i][j]=ans;
+            }
+        }
+     
+     return dp[n][target];
     }
 }
