694b336220076d1376beb6e0a233ba2aedf7b6ce
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2024-10-08 14:13:28 +0530

    Time: 9 ms (91.36%), Space: 42.4 MB (28.57%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     int fun(int i,int n,int k,int t,HashMap<Pair<Integer,Integer>,Integer> map){
         if(i==n){
             if(t==0) return 1;
@@ -17,5 +17,25 @@ class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         HashMap<Pair<Integer,Integer>,Integer> map=new HashMap<Pair<Integer,Integer>,Integer>();
         return fun(0,n,k,target,map);
+    }
+}
+class Solution {
+    int fun(int i,int n,int k,int t,Integer dp[][]){
+        if(i==n){
+            if(t==0) return 1;
+            return 0;
+        }
+        if(t<0)
+            return 0;
+        if(dp[i][t]!=null)
+            return dp[i][t];
+        int s=0;
+        for(int j=1;j<=k;j++)
+            s=(s+fun(i+1,n,k,t-j,dp))%(int)(1e9+7);
+        return dp[i][t]=s;
+    }
+    public int numRollsToTarget(int n, int k, int target) {
+        Integer dp[][]=new Integer[n+1][target+1];
+        return fun(0,n,k,target,dp);
     }
 }
