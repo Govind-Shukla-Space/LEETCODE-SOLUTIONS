6e369c4fd2fa002e80c3eb6a16027a5f928367e5
Govind-Shukla-Space <119527338+Govind-Shukla-Space@users.noreply.github.com>
2024-11-19 00:11:26 +0530

    Time: 0 ms (100.00%), Space: 16.7 MB (22.92%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -3,15 +3,32 @@ class Solution:
         l=[0]*len(code)
         if k==0:
             return l
-        q=0
-        while(q<len(code)):
-            s=0
-            if k>0:
-                for i1 in range(k):
-                    s+=code[(q+i1+1)%len(code)]
-            else:
-                for i1 in range(k*-1):
-                    s+=code[(q-i1-1)%len(code)]
-            l[q]=s
-            q+=1
+        s=0
+        n=len(code)
+        if k>0:
+            for i in range(1,k+1):
+                s+=code[i]
+            i=0
+            l[i]=s
+            j=k
+            i+=1
+            while i<n:
+                s=s-code[i]+code[(j+1)%n]
+                l[i]=s
+                i+=1
+                j+=1
+        else:
+            k = -k 
+            for i in range(k):
+                s += code[(n - k + i) % n]
+            i = 0
+            l[i] = s
+            j = n - k
+            i += 1
+            
+            while i < n:
+                s = s + code[(i - 1) % n] - code[(j) % n]
+                l[i] = s
+                i += 1
+                j += 1
         return l
